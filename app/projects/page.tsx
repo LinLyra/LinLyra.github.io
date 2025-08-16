17a54f41e7d2e476b1916423dbf0ec6f7a332825
Long Lin <2966752482@qq.com>
2025-08-16 19:20:34 +0800

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState } from "react"
-import { GalaxyBackground } from "@/components/galaxy-background"
 import { Navigation } from "@/components/navigation"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
@@ -67,7 +66,6 @@ export default function ProjectsPage() {
 
   return (
     <div className="relative min-h-screen">
-      <GalaxyBackground />
       <Navigation activeSection="projects" onSectionChange={() => {}} />
 
       <div className="relative z-10 pt-20 p-6">
