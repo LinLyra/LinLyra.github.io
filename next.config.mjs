7c7da3d48744a6dd0d17bb2d2a17b02c40049253
Long Lin <2966752482@qq.com>
2025-08-17 12:36:03 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',           // 关键：导出静态站点到 ./out
+  output: 'export',           // 关键：让 Next 导出静态站点
   images: { unoptimized: true },
-  // 可选
-  // trailingSlash: true,
-  reactStrictMode: true,
+  trailingSlash: true         // 生成 .../index.html，配合 GitHub Pages 更稳
 };
 export default nextConfig;
+
