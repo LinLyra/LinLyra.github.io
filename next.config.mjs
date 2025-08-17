23c6fabba3b253d6e4ddb665f899fc90458e20ea
Long Lin <2966752482@qq.com>
2025-08-17 12:00:34 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
-// next.config.mjs
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // GH Pages 专用的 output: "export" 要去掉
-  // images 你可以保留或删除；删除后可用 Vercel 的图片优化
-  images: { unoptimized: true }, // 可留可去
+  output: 'export',           // 关键：导出静态站点到 ./out
+  images: { unoptimized: true },
+  // 可选
+  // trailingSlash: true,
+  reactStrictMode: true,
 };
-
 export default nextConfig;
