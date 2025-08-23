8a6c1915b666063e26ceed2bd82e94c4300be5d8
Long Lin <2966752482@qq.com>
2025-08-23 11:53:36 +0800

    Update page.tsx

--- app/data/kaggle-ncaa-basketball/page.tsx
+++ app/data/kaggle-ncaa-basketball/page.tsx
@@ -33,7 +33,6 @@ export default function UpgradingProjectPage() {
             This project is currently being upgraded.  
           </p>
 
-          </div>
         </div>
       </main>
     </div>
