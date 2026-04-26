bd32f74ca9f3a917cb787b9d7d093f0a2a97f195
Lyra Lin <2966752482@qq.com>
2026-04-26 21:48:17 +1000

    4/26-1

--- components/ShipConsole.tsx
+++ components/ShipConsole.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { useEffect, useMemo, useState } from "react"
 
+import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
