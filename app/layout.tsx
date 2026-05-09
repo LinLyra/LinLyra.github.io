3bb40363f935b0c95afe79cd0347c0653da66f4f
Lyra Lin <2966752482@qq.com>
2026-05-09 16:05:57 +1000

    5/9-2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
+import { DeferredVisualEffects } from "@/components/deferred-visual-effects";
 import ShipConsoleLazy from "@/components/ship-console-lazy";
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -29,6 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     >
       <body>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          <DeferredVisualEffects />
           {children}
         </ThemeProvider>
         <ShipConsoleLazy />
